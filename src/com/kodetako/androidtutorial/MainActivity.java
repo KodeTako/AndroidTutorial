7e7241010267d0911af01830ae120d6c6416f071
Sergio Sáenz <tuanido8@gmail.com>
2014-07-22 20:28:11 +0200

    OnActivityResult example

--- src/com/kodetako/androidtutorial/MainActivity.java
+++ src/com/kodetako/androidtutorial/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kodetako.androidtutorial;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -7,35 +8,68 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
-	
+
 	private static final String TAG = "MainActivity";
 	public static final String MESSAGE_KEY = "message";
-	
-	private EditText	mTextToSend;
+	private static final int REQUEST_CODE_SEND_MESSAGE = 1;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        mTextToSend = (EditText) findViewById(R.id.text_to_send);
-        
-        Button nextButton = (Button) findViewById(R.id.nextButton);
-        nextButton.setOnClickListener(new OnClickListener() {
-			
+	private EditText 	mTextToSend;
+	private TextView	mResultText;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mTextToSend = (EditText) findViewById(R.id.text_to_send);
+		mResultText = (TextView) findViewById(R.id.result_text);
+
+		Button nextButton = (Button) findViewById(R.id.nextButton);
+		nextButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				sendTextToNextActivity();
 			}
 		});
-    }
-    
-    private void sendTextToNextActivity () {
-    	Intent i = new Intent(this, SecondActivity.class);
-    	i.putExtra(MESSAGE_KEY, mTextToSend.getText().toString());
-    	startActivity(i);
-    }
+	}
+
+	private void sendTextToNextActivity() {
+		Intent i = new Intent(this, SecondActivity.class);
+		i.putExtra(MESSAGE_KEY, mTextToSend.getText().toString());
+		startActivityForResult(i, REQUEST_CODE_SEND_MESSAGE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent intent) {
+		super.onActivityResult(requestCode, resultCode, intent);
+
+		switch (requestCode) {
+		case REQUEST_CODE_SEND_MESSAGE:
+			onSendMessageResult(resultCode, intent);
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void onSendMessageResult(int resultCode, Intent intent) {
+		switch (resultCode) {
+		case Activity.RESULT_OK:
+			String result = intent.getExtras().getString(SecondActivity.RESPONSE_KEY);
+			mResultText.setText(result);
+			break;
+		case Activity.RESULT_CANCELED:
+			Toast.makeText(MainActivity.this, R.string.toast_result_canceled, Toast.LENGTH_SHORT).show();
+			break;
+
+		default:
+			break;
+		}
+	}
 }
--- src/com/kodetako/androidtutorial/SecondActivity.java
+++ src/com/kodetako/androidtutorial/SecondActivity.java
@@ -1,30 +1,52 @@
 package com.kodetako.androidtutorial;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class SecondActivity extends ActionBarActivity {
-
+public class SecondActivity extends ActionBarActivity implements OnClickListener {
+	
+	public static final String RESPONSE_KEY = "response";
+	
+	private EditText mResponseMessage;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_second);
-		
+
 		String message = (String) getIntent().getExtras().get(MainActivity.MESSAGE_KEY);
 		TextView tv = (TextView) findViewById(R.id.result_text);
 		tv.setText(message);
+
+		mResponseMessage = (EditText) findViewById(R.id.response_message);
 		
-		Button finishButton = (Button) findViewById(R.id.finish_button);
-		finishButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				finish();
-			}
-		});
+		findViewById(R.id.cancel_button).setOnClickListener(this);
+		findViewById(R.id.response_button).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.cancel_button:
+			setResult(Activity.RESULT_CANCELED);
+			finish();
+			break;
+		case R.id.response_button:
+			Intent response = getIntent();
+			response.putExtra(RESPONSE_KEY, mResponseMessage.getText().toString());
+			setResult(Activity.RESULT_OK, response);
+			finish();
+			break;
+
+		default:
+			break;
+		}
+		
 	}
 }
