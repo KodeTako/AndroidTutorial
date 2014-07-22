fc6aaf0b1190b6dec5f4dd53927b13e4a356be59
Sergio Sáenz <tuanido8@gmail.com>
2014-07-22 19:25:48 +0200

    Example of text listener and text change.

--- src/com/kodetako/androidtutorial/MainActivity.java
+++ src/com/kodetako/androidtutorial/MainActivity.java
@@ -1,9 +1,11 @@
 package com.kodetako.androidtutorial;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,25 +14,20 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        
+        final TextView tv = (TextView) findViewById(R.id.bind_tv_example);
+        
+        EditText et = (EditText) findViewById(R.id.bind_et_example);
+        et.addTextChangedListener(new TextWatcher() {
+			
+			@Override public void onTextChanged(CharSequence s, int start, int before, int count) { }
+			
+			@Override public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				tv.setText(s);
+			}
+		});
     }
 }
